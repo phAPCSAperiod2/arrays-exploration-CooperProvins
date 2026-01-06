6da0c25d4a9136f5888e7ede9a9db1b19b6ac471
CooperProvins <cooperprovins@gmail.com>
2026-01-06 18:39:00 +0000

    Back button

--- src/App.java
+++ src/App.java
@@ -12,11 +12,11 @@ public class App {
         // create a list of top 5 things
         // --- TO DO: Change to your own list ---
         String[] top5 = {
-            "1. Coding",
-            "2. Music",
-            "3. Movies",
-            "4. Sports",
-            "5. Travel"
+            "1. Mango",
+            "2. Pineapple",
+            "3. Watermelon",
+            "4. Raspberry",
+            "5. Blackberry"
         };
 
          // A JFrame is a window where we can design our UI
@@ -26,12 +26,15 @@ public class App {
 
         // create a Button and a Label
         JButton nextButton = new JButton("Next");
+        JButton backButton = new JButton("Back");
         JLabel outputLabel = new JLabel();
+
         // --- TO DO: create a back button, format, and add it to the frame ---
 
         // place and size for components
         // setBounds(x position, y position, width, height)
         nextButton.setBounds(100, 200, 100, 50);
+        backButton.setBounds(250, 200, 100, 50);
         outputLabel.setBounds(100,100,200,50);
         outputLabel.setFont(new Font("Arial", Font.PLAIN, 32));
         outputLabel.setForeground(Color.BLUE);
@@ -47,6 +50,12 @@ public class App {
         nextButton.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
             currentIndex = getNextIndex(currentIndex, top5.length);
+            outputLabel.setText(top5[currentIndex]);
+        }    });
+
+        backButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            currentIndex = getPreviousIndex(currentIndex, top5.length);
             outputLabel.setText(top5[currentIndex]);
         }    });
 
@@ -80,6 +89,13 @@ public class App {
      * @param listLength
      * @return previous index
      */
-    
+    public static int getPreviousIndex(int currentIndex, int listLength) {
+            if (currentIndex == 0) {
+                return listLength-1; // wrap around to the end
+            }
+            else {
+                return currentIndex - 1; // move to the previous index
+            }
+        }
 }
 
